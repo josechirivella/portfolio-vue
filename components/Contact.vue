edd38300a8c5f1e1ed670169670335bc49e97655
Jose Chirivella <jrchg1403@gmail.com>
2021-11-11 00:23:34 -0500

    feat: added contact component

---
+++ components/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id='contactForm'>
+    <form class='card' @submit='submitForm'>
+      <input id='name' autocomplete='name' type='text' @input='contactForm.name'>
+      <input id='email' autocomplete='email' type='email' @input='contactForm.email'>
+      <textarea id='message' @input='contactForm.message'>
+      </textarea>
+      <button>
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contactForm: {
+        name: String,
+        email: String,
+        message: String
+      }
+    };
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      console.log('form', e);
+    }
+  }
+};
+</script>
