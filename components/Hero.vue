67fb1fe87468b33f4f2ca4cc01f7014816034b92
Jose Chirivella <jrchg1403@gmail.com>
2021-11-11 00:53:31 -0500

    feat: added hero component

---
+++ components/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id='hero' class='bg-black text-white'>
+    <h1>Jose Chirivella</h1>
+    <h3>Software Engineer</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
