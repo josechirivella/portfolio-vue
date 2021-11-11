5bc42966ec095e82690338932048edcbfb6675c9
Jose Chirivella <jrchg1403@gmail.com>
2021-11-11 00:52:53 -0500

    feat: added tailwind config file and @tailwindcss/typography package

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
