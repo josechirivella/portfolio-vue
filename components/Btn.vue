cbdceeb84a6b49e0e1e1879338f628f905e079b3
Jose Chirivella <jrchg1403@gmail.com>
2022-01-21 07:51:58 -0500

    feat: added btn component

---
+++ components/Btn.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="btn p-4 border-0 rounded-lg" :class="classList">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Btn',
+  props: {
+    classList: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
