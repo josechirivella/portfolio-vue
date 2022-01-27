6efafc7d4184ca2fcd2f88c21e08358a6e3621ef
Jose Chirivella <jrchg1403@gmail.com>
2022-01-27 17:41:55 -0500

    feat: added styling to the hero component

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id='hero' class='bg-black text-white'>
+  <div
+    id="hero"
+    class="
+      bg-black
+      text-white
+      min-h-screen
+      flex
+      items-center
+      flex-col
+      justify-center
+      text-lg
+    "
+  >
     <h1>Jose Chirivella</h1>
     <h3>Software Engineer</h3>
   </div>
