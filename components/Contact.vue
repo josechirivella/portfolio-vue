6514785d7d63f52b44c6080380d1d67166ea3237
Jose Chirivella <jrchg1403@gmail.com>
2022-09-11 23:11:07 -0400

    feat: added contact page

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,11 +1,11 @@
 <template>
   <div id='contactForm' class='border border-gray-300 rounded-lg p-4 w-1/4'>
     <form class='card flex flex-col' @submit='submitForm'>
-      <input id='name' v-model.trim='name' autocomplete='name' class='mb-4 p-2' placeholder='Your name'
-             type='text'>
-      <input id='email' v-model.trim='email' autocomplete='email' class='mb-4 p-2' placeholder='Your email'
-             type='email'>
-      <textarea id='message' v-model='message' class='p-2' placeholder='Enter your message'>
+      <input id="name" v-model.lazy.trim="name" autocomplete="name" class="mb-4 p-2" placeholder="Your name"
+             type="text">
+      <input id="email" v-model.lazy.trim="email" autocomplete="email" class="mb-4 p-2" placeholder="Your email"
+             type="email">
+      <textarea id="message" v-model.lazy="message" class="p-2" placeholder="Enter your message">
       </textarea>
       <button>
         Submit
@@ -26,7 +26,8 @@ export default {
   methods: {
     submitForm(e) {
       e.preventDefault();
-      // const {name, email, message} = this;
+      const { name, email, message } = this;
+      console.log({ name, email, message });
     }
   }
 };
