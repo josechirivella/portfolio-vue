a6c0023efeba92c347c3f59fb6f5fd506e4fdfed
Jose Chirivella <jrchg1403@gmail.com>
2024-07-31 13:48:00 -0500

    style: added about us content

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>About me</h1>
+  <div>
+    <h1>Hi, I'm Jose!</h1>
+    <p>I'm a Senior Software Engineer with over 10 years of experience, mostly front-end but I have joined the dark side in the past.
+    </p>
+  </div>
 </template>
+<script setup>
+</script>
