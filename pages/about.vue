ae3490ff610c3f3061c6f6fe5d8ee3147484b2b9
Jose Chirivella <jrchg1403@gmail.com>
2024-08-23 00:37:21 -0500

    chore: about text updates

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <h1>Hi, I'm Jose!</h1>
-    <p>I'm a Senior Software Engineer with over 10 years of experience, mostly front-end but I have joined the dark side in the past.
+    <h1 class="text-7xl mb-4">Hi, I'm Jose!</h1>
+    <p class="mb-4">I'm a Senior Software Engineer with over 10 years of experience, mostly front-end but I have joined the dark side in the past.
     </p>
+    <p class="mb-4">Originally, from Venezuela. Was studying Computer Engineering when my family had the change to migrate to the US in 2014. Quickly decided that I wasn't going to spent my parents retirement,
+      instead, worked really hard to pay myself a Coding School called The Iron Yard in 2015. After that, my career launched working for a couple of startups like Windsor Circle, MATI Energy, and others...
+      Also, worked for a top 500 company called Cisco Systems 😮‍💨.
+    </p>
+    <p class="mb-4">I have become an expert in Vue and Angular. This blog was written in Nuxt. I have used Rails in the past and Laravel for my previous experience.</p>
   </div>
 </template>
 <script setup>
