4a665c7c2037506c5764e18e694923325c8d0799
Jose Chirivella <jrchg1403@gmail.com>
2024-10-28 17:25:13 -0500

    style: added new blog layout

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,16 +1,30 @@
 <template>
-  <div>
+  <div class="max-w-2xl mx-auto">
     <h1 class="font-bold text-4xl">Hi! I'm Jose</h1>
-    <p>I'm a Software Engineer with a focus in Front End. Here you will find my thoughts.</p>
-    <div class="pages--container py-4 grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div v-for="(post, index) in posts" :key="index" class="max-w-sm rounded-lg border-gray-800 border p-4 overflow-hidden shadow-lg">
+    <p class="mb-4">
+      I'm a Software Engineer with a focus in Front End. Here you will find my
+      thoughts.
+    </p>
+    <div class="pages--container py-4 grid grid-cols-1 gap-4">
+      <div v-for="(post, index) in posts" :key="index">
         <nuxt-link :to="post._path">
-          <img v-if="post.image" :src="post.image" alt="Post cover" loading="lazy" class="w-full">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl text-white mb-2">{{post.title}}</div>
-            <p class="text-gray-100 text-base">{{post.description}}</p>
-          </div>
-          <BlogTags :tags="post.tags"></BlogTags>
+          <Card class="overflow-hidden">
+            <template v-if="post.image" #header>
+              <img
+                :src="post.image"
+                alt="Post cover"
+                loading="lazy"
+                class="w-full h-48 object-cover"
+              />
+            </template>
+            <template #title>{{ post.title }}</template>
+            <template #content>
+              {{ post.description }}
+            </template>
+            <template v-if="post.tags" #footer>
+              <div class="flex"></div>
+            </template>
+          </Card>
         </nuxt-link>
       </div>
     </div>
@@ -18,5 +32,7 @@
 </template>
 
 <script lang="ts" setup>
-const {data: posts} = await useAsyncData('posts', () => queryContent('/').find())
+const { data: posts } = await useAsyncData('posts', () =>
+  queryContent('/').find()
+);
 </script>
