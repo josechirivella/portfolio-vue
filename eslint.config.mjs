b9bcc8f4c99e91beaf0f692a292ac96dff4dd540
Jose Chirivella <jrchg1403@gmail.com>
2024-11-04 23:14:15 -0600

    fix: added missing eslint plugins and configured them

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,18 @@
 import eslintConfigPrettier from 'eslint-config-prettier';
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import pluginVue from 'eslint-plugin-vue';
+import pluginJest from 'eslint-plugin-jest';
 
 export default withNuxt(
+  eslintConfigPrettier,
+  ...pluginVue.configs['flat/recommended'],
+  pluginJest.configs['flat/recommended'],
   {
+    files: ['**/*.vue'],
     rules: {
       'vue/multi-word-component-names': 'off',
     },
     ignores: ['node_modules', '.nuxt', 'dist', 'public'],
   },
-  eslintConfigPrettier
 )
