25e7fed5031ee699459414c70adc831fbfb008b8
Jose Chirivella <jrchg1403@gmail.com>
2024-12-18 18:39:50 -0600

    feat: added logic for og meta type to be profile for the about page

--- pages/about.vue
+++ pages/about.vue
@@ -9,19 +9,26 @@
       Originally, from Venezuela. Was studying Computer Engineering when my
       family had the chance to migrate to the US in 2014. Quickly decided that I
       wasn't going to spent my parents retirement, instead, worked really hard
-      to pay myself a Coding School called The Iron Yard in 2014. After graduation, my
-      career launched working for a couple of startups like Windsor Circle, MATI
-      Energy, and others... Also, worked for a top 500 company called Cisco
-      Systems 😮‍💨
+      to pay myself a Coding School called The Iron Yard in 2014. After
+      graduation, my career launched working for a couple of startups like
+      Windsor Circle, MATI Energy, and others... Also, worked for a top 500
+      company called Cisco Systems 😮‍💨
     </p>
     <p>
-      I have become an expert in Vue and Angular.
-      I have used Rails in the past and Laravel.
+      I have become an expert in Vue and Angular. I have used Rails in the past,
+      Laravel and NodeJS
     </p>
     <h3>This site</h3>
-    This website was written with Nuxt and TailwindCSS. I'm using Vercel to
-    deploy it and <a href="https://pnpm.io/" target="_blank">pnpm</a> as a
-    dependency manager
+    This website was written with Nuxt, TailwindCSS and PrimeVue for components.
+    I'm using Vercel to deploy it and
+    <a
+      href="https://pnpm.io/"
+      target="_blank"
+    >pnpm</a> as a dependency manager
   </div>
 </template>
-<script setup></script>
+<script lang="ts" setup>
+useSeoMeta({
+  ogType: 'profile',
+});
+</script>
