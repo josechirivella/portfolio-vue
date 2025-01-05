4c7fdff27fa512f44a1d631e25a49ab91b601f1a
Jose Chirivella <jrchg1403@gmail.com>
2025-01-05 00:41:31 -0600

    feat: added new Table of contents component

---
+++ components/blog/Toc.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2 class="text-lg font-semibold">
+    Table of contents
+  </h2>
+  <ul class="list-none">
+    <li
+      v-for="link of toc.links"
+      :key="link.id"
+    >
+      <a :href="`#${link.id}`">{{ link.text }}</a>
+      <ul
+        v-if="link.children"
+        class="list-none"
+      >
+        <li
+          v-for="child of link.children"
+          :key="child.id"
+        >
+          <a :href="`#${child.id}`">{{ child.text }}</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  toc: Object,
+});
+</script>
