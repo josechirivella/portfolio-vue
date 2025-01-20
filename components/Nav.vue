d06565021ed261d25669010db843dd3c5c622a80
Jose Chirivella <jrchg1403@gmail.com>
2025-01-20 13:42:41 -0600

    fix: added click handler when navbar is clicked on mobile to hide the navbar

--- components/Nav.vue
+++ components/Nav.vue
@@ -38,6 +38,7 @@
               :target="item.target"
               :to="item.link"
               class="px-3 py-2 flex items-center text-lg font-bold leading-snug"
+              @click="toggleNavbar()"
             >
               <i class="text-lg leading-lg text-white opacity-75" /><span
                 class="ml-2 text-gray-400 hover:text-white"
