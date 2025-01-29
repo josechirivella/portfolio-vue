b66fc26de13428e41d8ea9d1643094ff858dda15
Jose Chirivella <jrchg1403@gmail.com>
2025-01-29 00:17:17 -0600

    fix: removed production checks and added pageLeave and pageView log for analytics

--- plugins/posthog.client.js
+++ plugins/posthog.client.js
@@ -1,27 +1,29 @@
 import { defineNuxtPlugin } from '#app';
 import posthog from 'posthog-js';
 
-const production = import.meta.env.MODE === 'production';
+const runtimeConfig = useRuntimeConfig();
 
 export default defineNuxtPlugin(() => {
-  const runtimeConfig = useRuntimeConfig();
   const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
     api_host: runtimeConfig.public.posthogHost || 'https://app.posthog.com',
     capture_pageview: false, // we add manual pageview capturing below
     loaded: (posthog) => {
-      if (!production) {
-        // posthog.debug();
-        posthog.opt_out_capturing();
-        posthog.set_config({ disable_session_recording: true });
-      }
+      // posthog.debug();
+      posthog.opt_out_capturing();
+      posthog.set_config({ disable_session_recording: true });
     },
   });
 
   // Make sure that pageviews are captured with each route change
   const router = useRouter();
-  router.afterEach((to) => {
+  router.afterEach((to, from) => {
     nextTick(() => {
-      if (!production) return;
+      if (from.fullPath !== to.fullPath) {
+        posthog.capture('$pageleave', {
+          current_url: from.fullPath,
+        });
+      }
+
       posthog.capture('$pageview', {
         current_url: to.fullPath,
       });
