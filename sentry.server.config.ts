ca064fed2d8895e3ed744f45bb6ad96d9629c0b0
Jose Chirivella <jrchg1403@gmail.com>
2025-02-03 12:35:15 -0600

    fix: added conditional to check if sentry env is loaded

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,12 +1,14 @@
-import * as Sentry from "@sentry/nuxt";
- 
-Sentry.init({
-  dsn: "https://4b6d740a23ac182df208104fdaedce13@o4508702962548736.ingest.us.sentry.io/4508702965301248",
+import * as Sentry from '@sentry/nuxt';
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-  
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: 'https://4b6d740a23ac182df208104fdaedce13@o4508702962548736.ingest.us.sentry.io/4508702965301248',
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
