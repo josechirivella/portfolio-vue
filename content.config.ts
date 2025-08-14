116a8e5506f05ca7a949e5ba8f01afcc42d1e306
Jose Chirivella <jrchg1403@gmail.com>
2025-08-14 01:03:13 -0400

    feat: upgraded nuxt/content to v3 * updated api calls

---
+++ content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, defineContentConfig, z } from '@nuxt/content';
+
+export default defineContentConfig({
+  collections: {
+    content: defineCollection({
+      type: 'page',
+      source: '**/*.md',
+      schema: z.object({
+        date: z.string(),
+        tags: z.array(z.string()),
+        image: z.string(),
+      }),
+    }),
+  },
+});
