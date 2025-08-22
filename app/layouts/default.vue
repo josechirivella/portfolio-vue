d0a2f368fa73f21b4f4df11a9d3aaa712024cd5d
Jose Chirivella <jrchg1403@gmail.com>
2025-08-22 10:37:13 -0400

    feat: integrate `SpeedInsights` component into default layout and update meta attributes

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,12 +4,15 @@
     <main class="container mx-auto p-4">
       <slot />
     </main>
+    <SpeedInsights />
   </div>
 </template>
 <script lang="ts" setup>
+import { SpeedInsights } from '@vercel/speed-insights/nuxt';
+
 useHead({
   htmlAttrs: {
-    class: 'dark-mode-always'
-  }
-})
+    class: 'dark-mode-always',
+  },
+});
 </script>
